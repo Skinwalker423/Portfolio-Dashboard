65242f95d6f71407fa8467e2ca66a5a1be682b57
Skinwalker423 <85347852+Skinwalker423@users.noreply.github.com>
2022-08-07 18:19:45 -0700

    navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,8 +1,27 @@
-import React from 'react'
+import React, {useEffect} from 'react';
+import {AiOutlineMenu } from 'react-icons/ai';
+import { FiShoppingCart } from 'react-icons/fi';
+import { BsChatLeft } from 'react-icons/bs';
+import { RiNavigationLine } from 'react-icons/ri';
+import { MdKeyboardArrowDown } from 'react-icons/md';
+import { TooltipComponent } from '@syncfusion/ej2-react-popups';
+
+import avatar from '../../data/avatar.jpg';
+import {Cart, Chat, Notification, UserProfile} from '../../components';
+
+import { useStateContext } from '../../contexts/ContextProvide';
 
 const Navbar = () => {
   return (
-    <div>Navbar</div>
+    <div>
+      <TooltipComponent>
+          <BsChatLeft />
+          <MdKeyboardArrowDown />
+          <AiOutlineMenu  />
+          <RiNavigationLine />
+          <FiShoppingCart/>
+      </TooltipComponent>
+    </div>
   )
 }
 
